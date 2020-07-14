a9a0de98bc03b8a84f1cd112e614a8dc1c49659b
Alexey Korchagin <akorchaginn@gmail.com>
2020-07-15 03:53:50 +0700

    Расчёт ачивок по навыку говорения

--- site.local/app/Listeners/AchievementExcellentTalkative.php
+++ site.local/app/Listeners/AchievementExcellentTalkative.php
@@ -4,8 +4,14 @@
 namespace App\Listeners;
 
 
+use App\Entity\Achievement;
+use App\Entity\Skill;
 use App\Events\AchievementCalculatorEvent;
 
+/**
+ * Class AchievementExcellentTalkative
+ * @package App\Listeners
+ */
 class AchievementExcellentTalkative
 {
     /**
@@ -16,6 +22,30 @@ class AchievementExcellentTalkative
      */
     public function handle(AchievementCalculatorEvent $event)
     {
-        logs()->info(self::class);
+        $student = $event->getStudent();
+
+        $talkativeTasksCount = $student->tasks()
+            ->getQuery()
+            ->join('task_skill', 'task.id', 'task_skill.task_id')
+            ->join('skill', 'skill.id', 'task_skill.skill_id')
+            ->where('skill.type', Skill::TALKATIVE_TYPE_CODE)
+            ->get()->count();
+
+        $allTalkativeTasks = Skill::query()
+                            ->join('task_skill', 'skill.id', 'task_skill.skill_id')
+                            ->join('task', 'task.id', 'task_skill.task_id')
+                            ->where('type', Skill::TALKATIVE_TYPE_CODE)
+                            ->get()->count();
+
+        if ($talkativeTasksCount === $allTalkativeTasks) {
+            $excellentTalkative = Achievement::query()->where('discriminator', self::class)->first();
+
+            $student->achievements()->newQuery()->where('id', $excellentTalkative->id)->existsOr(function () use ($student, $excellentTalkative) {
+                $student->achievements()->attach($excellentTalkative->id, [
+                    'completed_date' => now(),
+                ]);
+            });
+        }
+
     }
 }
